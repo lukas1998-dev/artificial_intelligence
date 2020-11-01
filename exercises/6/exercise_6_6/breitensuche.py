ee3b303b95a04684e80909b9c3d9ba3db5e3ee15
ld-183106 <lukas.daibert@rwu.de>
2020-11-01 11:53:01 +0100

    Calculated the possible moves

--- exercises/6/exercise_6_6/breitensuche.py
+++ exercises/6/exercise_6_6/breitensuche.py
@@ -2,15 +2,39 @@
 def printPuzzle(puzzle):
 
     for i in puzzle:
-        if(i%3 == 0):
+        if((puzzle.index(i)+1)%3 == 0):
             print(i)
         else:
             print(i, end='')
     print()
 
+def calcNodes(puzzle):
+
+    index = puzzle.index(0)
+
+    if((index % 2) == 0):
+        if(index == 4):
+            return 4
+        else:
+            return 2
+    else:
+        return 3
+
+def moveTile(puzzle, moves):
+    
+
+def breadth_first(puzzle, goal):
+    if(puzzle == goal):
+        print("Finished")
+        return;
+    else:
+        possibleMoves = calcNodes(puzzle)
+        moveTile(puzzle, possibleMoves)
+
+
 #---Main---
 
-puzzle = [1,2,3,4,5,6,7,8,0]
+puzzle = [1,2,3,6,0,6,7,9,8]
 #print(" Input vals from 0-8 for start state ")
 #for i in range(0,9):
 #    x = int(input("enter vals :"))
@@ -28,3 +52,5 @@ printPuzzle(puzzle)
 
 print("End puzzle:")
 printPuzzle(goal)
+
+breadth_first(puzzle, goal)
