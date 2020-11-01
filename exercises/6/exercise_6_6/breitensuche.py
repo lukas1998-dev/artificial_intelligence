71099de43b7763b428c604134ec3e57002ebf121
ld-183106 <lukas.daibert@rwu.de>
2020-11-01 10:54:05 +0100

    Added start puzzle and end puzzle for breitensuche;

---
+++ exercises/6/exercise_6_6/breitensuche.py
@@ -0,0 +1,15 @@
+
+#---Main---
+
+puzzle = []
+print(" Input vals from 0-8 for start state ")
+for i in range(0,9):
+    x = int(input("enter vals :"))
+    puzzle.append(x)
+
+ # User input of goal state
+goal = []
+print(" Input vals from 0-8 for goal state ")
+for i in range(0,9):
+    x = int(input("Enter vals :"))
+    goal.append(x)
