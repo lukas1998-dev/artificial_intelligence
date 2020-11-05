ac44f1e57eeb0b3104e922c28dcbe34f65f3b8bd
ld-183106 <lukas.daibert@rwu.de>
2020-11-05 10:38:33 +0100

    Changed the structure to a doubly linked list

--- exercises/6/exercise_6_6/breitensuche.py
+++ exercises/6/exercise_6_6/breitensuche.py
@@ -1,47 +1,37 @@
 
+class Node:
+    def __init__(self, next=None, prev=None, data=None):
+        self.next = next
+        self.prev = prev
+        self.data = data
+
 def printPuzzle(puzzle):
 
-    for i in puzzle:
-        if((puzzle.index(i)+1)%3 == 0):
+    for i in puzzle.data:
+        if((puzzle.data.index(i)+1)%3 == 0):
             print(i)
         else:
             print(i, end='')
     print()
 
-def calcNodes(puzzle):
-
-    index = puzzle.index(0)
-
-    if((index % 2) == 0):
-        if(index == 4):
-            return 4
-        else:
-            return 2
-    else:
-        return 3
-
-def moveTile(puzzle, moves):
-    
 
 def breadth_first(puzzle, goal):
-    if(puzzle == goal):
+    if(puzzle.data == goal.data):
         print("Finished")
         return;
     else:
-        possibleMoves = calcNodes(puzzle)
-        moveTile(puzzle, possibleMoves)
+        print("TODO")
 
 
 #---Main---
-
-puzzle = [1,2,3,6,0,6,7,9,8]
+puzzle = Node(next=None, prev=None, data=[1,2,3,4,5,6,7,0,8])
 #print(" Input vals from 0-8 for start state ")
 #for i in range(0,9):
 #    x = int(input("enter vals :"))
 #    puzzle.append(x)
 
  # User input of goal state
-goal = [1,2,3,4,5,6,7,8,0]
+goal = Node(next=None, prev=None, data=[1,2,3,4,5,6,7,8,0])
 #print(" Input vals from 0-8 for goal state ")
 #for i in range(0,9):
 #    x = int(input("Enter vals :"))
