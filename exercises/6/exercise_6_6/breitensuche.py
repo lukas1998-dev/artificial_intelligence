d72c7e00b558d292ab7cdfd0a3a2b84b0050a77d
ld-183106 <lukas.daibert@rwu.de>
2020-11-15 22:43:58 +0100

    Changed to a different list structure

--- exercises/6/exercise_6_6/breitensuche.py
+++ exercises/6/exercise_6_6/breitensuche.py
@@ -13,10 +13,8 @@ def printList(list):
 def printPuzzle(puzzle):
 
     for i in puzzle.data:
-        if((puzzle.data.index(i)+1)%3 == 0):
             print(i)
-        else:
-            print(i, end='')
+
     print()
 
 def breadth_first(puzzle, goal):
@@ -27,20 +25,20 @@ def breadth_first(puzzle, goal):
         list = [];
         list.append(puzzle);
 
-        while(True):
+        #while(True):
 
 
 #---Main---
 #TODO: Manual Input
 
-puzzle = Node(next=None, prev=None, data=[1,2,3,4,5,6,7,0,8])
+puzzle = Node(next=None, prev=None, data=[[1,2,3],[4,5,6],[7,0,8]])
 #print(" Input vals from 0-8 for start state ")
 #for i in range(0,9):
 #    x = int(input("enter vals :"))
 #    puzzle.append(x)
 
  # User input of goal state
-goal = Node(next=None, prev=None, data=[1,2,3,4,5,6,7,8,0])
+goal = Node(next=None, prev=None, data=[[1,2,3],[4,5,6],[7,8,0]])
 #print(" Input vals from 0-8 for goal state ")
 #for i in range(0,9):
 #    x = int(input("Enter vals :"))
