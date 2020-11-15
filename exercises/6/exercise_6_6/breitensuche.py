770fda60911f1f41820eda69576eff91955a9ad3
ld-183106 <lukas.daibert@rwu.de>
2020-11-15 22:16:57 +0100

    Added list of puzzles

--- exercises/6/exercise_6_6/breitensuche.py
+++ exercises/6/exercise_6_6/breitensuche.py
@@ -5,6 +5,11 @@ class Node:
         self.prev = prev
         self.data = data
 
+def printList(list):
+    for e in list:
+        printPuzzle(e);
+
+
 def printPuzzle(puzzle):
 
     for i in puzzle.data:
@@ -14,16 +19,20 @@ def printPuzzle(puzzle):
             print(i, end='')
     print()
 
-
 def breadth_first(puzzle, goal):
     if(puzzle.data == goal.data):
-        print("Finished")
+        print("Goal reached")
         return;
     else:
-        print("TODO")
+        list = [];
+        list.append(puzzle);
+
+        while(True):
 
 
 #---Main---
+#TODO: Manual Input
+
 puzzle = Node(next=None, prev=None, data=[1,2,3,4,5,6,7,0,8])
 #print(" Input vals from 0-8 for start state ")
 #for i in range(0,9):
@@ -44,3 +53,4 @@ print("End puzzle:")
 printPuzzle(goal)
 
 breadth_first(puzzle, goal)
+print("Finished")
