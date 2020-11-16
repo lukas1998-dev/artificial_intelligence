b530f5a47b3641fdc98aafe72ad0d98aa59245bc
ld-183106 <lukas.daibert@rwu.de>
2020-11-16 14:41:10 +0100

    Finished breitensuche

--- exercises/6/exercise_6_6/breitensuche.py
+++ exercises/6/exercise_6_6/breitensuche.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+iteration = 0;
 
 class Node:
     def __init__(self, next=None, prev=None, data=None):
@@ -5,18 +8,47 @@ class Node:
         self.prev = prev
         self.data = data
 
-def switchTiles(direction, puzzle):
+def switchTiles(direction, newData):
     # switch direction of 0
     # direction 0 == left; 1 == right
     #           2 == up; 3 == down
+    newPuzzle = Node(data = deepcopy(newData));
+
+    if(direction == 0):
+        for row in newPuzzle.data:
+            if(0 in row):
+                zero = row.index(0);
+                row[zero], row[zero-1] = row[zero-1], row[zero]
+                break;
+
+    if(direction == 1):
+        for row in newPuzzle.data:
+            if(0 in row):
+                zero = row.index(0);
+                row[zero], row[zero+1] = row[zero+1], row[zero]
+                break;
+
+    if(direction == 2):
+        for row in newPuzzle.data:
+            if(0 in row):
+                zero = row.index(0);
+                col = newPuzzle.data.index(row);
+                newPuzzle.data[col][zero], newPuzzle.data[col-1][zero] =newPuzzle.data[col-1][zero], newPuzzle.data[col][zero]
+                break;
+
+    if(direction == 3):
+        for row in newPuzzle.data:
+            if(0 in row):
+                zero = row.index(0);
+                col = newPuzzle.data.index(row);
+                newPuzzle.data[col][zero], newPuzzle.data[col+1][zero] =newPuzzle.data[col+1][zero], newPuzzle.data[col][zero]
+                break;
+
+    return newPuzzle;
 
 
-    return;
 
 def expand(puzzle):
-    print("Expanding:");
-    printPuzzle(puzzle);
-
     newNodes = [];
 
     row = None
@@ -27,23 +59,22 @@ def expand(puzzle):
             col = puzzle.data.index(list);
             row = list.index(0)
 
-
     if(row == 0):
-        newNodes.append(switchTiles(1, puzzle));
+        newNodes.append(switchTiles(1, puzzle.data));
     if(row == 1):
-        newNodes.append(switchTiles(1, puzzle));
-        newNodes.append(switchTiles(0, puzzle));
+        newNodes.append(switchTiles(1, puzzle.data));
+        newNodes.append(switchTiles(0, puzzle.data));
     if(row == 2):
-        newNodes.append(switchTiles(0, puzzle));
+        newNodes.append(switchTiles(0, puzzle.data));
 
 
     if(col == 0):
-        newNodes.append(switchTiles(3, puzzle));
+        newNodes.append(switchTiles(3, puzzle.data));
     if(col == 1):
-        newNodes.append(switchTiles(3, puzzle));
-        newNodes.append(switchTiles(2, puzzle));
+        newNodes.append(switchTiles(2, puzzle.data));
+        newNodes.append(switchTiles(3, puzzle.data));
     if(col == 2):
-        newNodes.append(switchTiles(2, puzzle));
+        newNodes.append(switchTiles(2, puzzle.data));
 
     return newNodes;
 
@@ -52,7 +83,6 @@ def printList(list):
     for e in list:
         printPuzzle(e);
 
-
 def printPuzzle(puzzle):
     for i in puzzle.data:
         print(i)
@@ -60,16 +90,22 @@ def printPuzzle(puzzle):
 
 def breadth_first(list, goal):
 
-    newNodes = [];
+    global iteration;
+
+    printList(list)
     for p in list:
         if(p.data == goal.data):
             print("Goal reached");
             printPuzzle(p);
             return;
 
-        newNodes.append(expand(p));
+    newNodes = [];
+    for p in list:
+        newNodes.extend(expand(p));
 
     if(len(newNodes) > 0 ):
+        iteration +=1;
+        print("------------" +str(iteration) + "------------------")
         breadth_first(newNodes, goal);
     else:
         print("keine Lösung");
@@ -78,7 +114,7 @@ def breadth_first(list, goal):
 #---Main---
 #TODO: Manual Input
 
-puzzle = Node(next=None, prev=None, data=[[1,2,3],[4,5,6],[7,0,8]])
+puzzle = Node(next=None, prev=None, data=[[0,2,3],[1,4,5],[7,8,6]])
 #print(" Input vals from 0-8 for start state ")
 #for i in range(0,9):
 #    x = int(input("enter vals :"))
@@ -100,5 +136,6 @@ printPuzzle(goal)
 list = []
 list.append(puzzle)
 
+print("----------Starting-------------")
 breadth_first(list, goal)
 print("Finished")
